6de7500feb5b80f7a45c90f122646b4622c31587
ungarmichael <ungarmichael.mail@gmail.com>
2022-06-20 22:15:36 +0200

    chore: aud ghaklt

--- src/logic.ts
+++ src/logic.ts
@@ -1,30 +1,38 @@
 import { DataSource } from "typeorm";
 import { Garden } from "@entities/Garden";
 import { Plant } from "@entities/Plant";
+import { Field } from "@entities/Field";
 
-export default async function generatePlan(dbContext: DataSource, garden: Garden): Promise<void> {
-  
-    //create sample 2d array - copilot du gschissena, dua moi wos
-//   for(let i = 0; i < garden2dArray.length; i++) {
-//     for(let j = 0; j < garden2dArray[i].length; j++) {
-//         if(garden2dArray[i][j] != -1){
-//             Plant p = getSuitingPlant();
-            
-//         } 
-//     }
-//   }
+export default async function generateGardenFromRawGarden2dArray(
+  dbContext: DataSource,
+  rawGarden2dArray: number[][]
+): Promise<void> {
+  const garden = new Garden();
+  garden.size = rawGarden2dArray.length;
 
-garden = new Garden();
+  const tempFields: Field[] = [];
+  for (let i = 0; i < rawGarden2dArray.length; i++) {
+    for (let j = 0; j < rawGarden2dArray.length; j++) {
+      var newField: Field = new Field();
+      newField
+      try {
+        if (rawGarden2dArray[i][j] != -1) {
+          var p = getSuitingPlant(dbContext);
+        }
+      } catch (e) {}
+    }
+  }
+
+  // garden = new Garden();
 
   garden.size = 5;
   await garden.save();
 }
 
 function getSuitingPlant(db: DataSource): Plant {
-    db.getRepository(Plant).findOneBy
-    var plant: Plant = new Plant();
-    //find plant with index y in database
+  // db.getRepository(Plant).findOne
+  var plant: Plant = new Plant();
+  //find plant with index y in database
 
-
-    return new Plant();
+  return new Plant();
 }
