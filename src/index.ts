9bef3aaf2d660d2439f5571fbc1c8c3d56c587da
littlegamer <jneissl27@gmail.com>
2022-06-20 23:56:29 +0200

    Update logic
    Some logic was added to check that the input array is actually square, a rudimentary method for weighting plants was implemented as well.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import { Field } from "@entities/Field";
-import { Garden } from "@entities/Garden";
-import { DataSource } from "typeorm";
-import { AppDataSource } from "./data-source";
+import {DataSource} from "typeorm";
+import {AppDataSource} from "./data-source";
 import generateGardenFromRawGarden2dArray from "./logic";
-import generatePlan from "./logic";
 import seedDb from "./seed";
 
 const main = async (): Promise<DataSource> => {
   const app: DataSource = await AppDataSource.initialize();
 
   //pflonzn eineschnoitzn
-  seedDb(app);
+  await seedDb(app);
 
   //garden can only be as long as wide
   const rawGarden2dArray:number[][] = [
