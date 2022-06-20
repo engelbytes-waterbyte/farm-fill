3694f8a8d4290fca6a6a338a8e926c49f6ae7077
EinboeckFranz <EinboeckF180011@sus.htl-grieskirchen.at>
2022-06-20 11:46:05 +0200

    Updated CLI.

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,35 @@
 #!/usr/bin/env node
 import yargs from "yargs";
 
-// let OPTIONS = yargs
-//   .usage("\nUsage: {entity} {action}")
-//   .strict()
-
-// (async () => {
-//   yargs.parse();
-// })().catch((err) => {
-//   console.error(err);
-// });
+yargs
+    .scriptName('clitool4Farmfill')
+    .usage("$0 run [args]")
+    .command('run [options]', 'Run the script', (_) => {
+    }, (argv) => {
+        if(argv.benchmark) {
+            console.log('Benchmark')
+        }
+        if(argv.plans) {
+            console.log(`plans ${argv.plans}`)
+        }
+        if(argv.years) {
+            console.log(`years ${argv.years}`)
+        }
+    })
+    .option('benchmark', {
+        alias: 'b',
+        type: 'boolean',
+        description: 'Run in Benchmark Mode'
+    })
+    .option('plans', {
+        alias: 'p',
+        type: 'number',
+        description: 'Run the tool for X plans (for the eventually specified years)'
+    })
+    .option('years', {
+        alias: 'y',
+        type: 'number',
+        description: 'Run the tool for X years'
+    })
+    .help()
+    .argv;
